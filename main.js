4c6e8226bcaf5b4707f758274c7d267b18cf34e3
englishjourney <languageprof.mica@gmail.com>
2026-08-10 21:57:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ import { openProfile } from './profile.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const session = getSession();
-    
-document.getElementById('btn-avatar').addEventListener('click', () => {
-    openProfile();
-});
+
+    // Controle de exibição (Logado vs Deslogado)
     if (session) {
         // Usuário logado
         document.getElementById('auth-screen').classList.add('hidden');
@@ -19,7 +17,12 @@ document.getElementById('btn-avatar').addEventListener('click', () => {
         document.getElementById('app-screen').classList.add('hidden');
     }
 
-    // Toggle Formulários
+    // Event listener do Avatar (Perfil)
+    document.getElementById('btn-avatar').addEventListener('click', () => {
+        openProfile();
+    });
+
+    // Toggle Formulários (Login / Cadastro)
     document.getElementById('btn-show-register').addEventListener('click', () => {
         document.getElementById('login-form').parentElement.classList.add('hidden');
         document.getElementById('register-box').classList.remove('hidden');
@@ -36,7 +39,7 @@ document.getElementById('btn-avatar').addEventListener('click', () => {
         window.location.reload(); // Recarrega a página para voltar ao login
     });
 
-    // Lógica do Dialog
+    // Lógica do Dialog (Janelas)
     const modal = document.getElementById('content-modal');
     document.getElementById('btn-missions').addEventListener('click', () => modal.showModal());
     document.getElementById('btn-close-modal').addEventListener('click', () => modal.close());
