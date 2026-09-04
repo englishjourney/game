43b1730746b91be015ad5010c9d2f556e52c2cfb
englishjourney <languageprof.mica@gmail.com>
2026-09-04 18:36:09 -0300

    Update APPS_SCRIPT_URL to new endpoint

--- Yuki/script.js
+++ Yuki/script.js
@@ -1,7 +1,7 @@
 // Configurações do Supabase e Apps Script
 const SUPABASE_URL = "https://rmsmamzutvxugdbiqsrz.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_hMNCps2v2Odflpq9zDt_dw_Cgb_Jcxx";
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZDDcUVRJsSq9L-uFkKu9Hg9-ywAqqA3xJzHLWqn9Lg-qUEyKruOEGBgijQbbfmikOag/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz8dqyGcdAZklwHGu38wcndP-GfLa50nHObFKYHBj8HnRVzSkJpfoDpUgbo3KsspAPTwQ/exec";
 
 // Instância com nome 'supabaseClient' para evitar conflito com a biblioteca global
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
