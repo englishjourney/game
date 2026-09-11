1379545000bf0c03ccf53e5a0942d0c5ba4c004c
englishjourney <languageprof.mica@gmail.com>
2026-09-11 09:51:45 -0300

    Change Apps Script URL in script.js
    Updated the Apps Script URL for the project.

--- Yuki/script.js
+++ Yuki/script.js
@@ -1,7 +1,7 @@
 // Configurações do Supabase e Apps Script
 const SUPABASE_URL = "https://rmsmamzutvxugdbiqsrz.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_hMNCps2v2Odflpq9zDt_dw_Cgb_Jcxx";
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyzOTK6jnB2-u4uienT9nP8noT7CM95NzqUaAx5c3-LBluFSVZKJz7J2g7lItuZS4wBoQ/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxXXz6iSAqGWNrajf0UVPsof62W8Z2ZkGZLdy5SjUb4D8wd8c_k_sxgdyVoD9L_LbAM7Q/exec";
 
 // Instância com nome 'supabaseClient' para evitar conflito com a biblioteca global
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
